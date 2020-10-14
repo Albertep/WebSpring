f44d6f9f97a4f8553cdd5683b6b052ec0aeefcfb
Albertep <alberte9494@gmail.com>
2020-10-14 15:27:45 +0200

    Initial commit

---
+++ src/main/java/TutorialWeb/WebSpring/FastWebSpringApplication.java
@@ -0,0 +1,13 @@
+package TutorialWeb.WebSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastWebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TutorialWeb/WebSpring/FastWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package TutorialWeb.WebSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
